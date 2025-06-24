df45394b65da7620ada235407ac7ce00610daa89
Sasshin <krzeminska.laura@gmail.com>
2025-06-24 18:47:31 +0100

    Rewrote logic, switched key and values in seed_map, added checks for planting seeds

--- flower_farm.py
+++ flower_farm.py
@@ -4,10 +4,12 @@ import time  # for sleep function
 # This is a simple text-based game where you can plant flowers, wait for them to grow,
 # deliver them in bouquets and sell them at the market. You can also buy tools to help
 # you farming and sell more efficiently.
-inventory: dict = {"🌷🌱 Tulip seed": "", "🌼🌱 Daisy seed": "", "🌹🌱 Rose seed": "",
-                   "Tulip": 0, "Daisy": 0, "Rose": 0}
+
+# inventory: dict = {"🌷🌱 Tulip seed": 0, "🌼🌱 Daisy seed": 0, "🌹🌱 Rose seed": 0,
+#                    "Tulip": 0, "Daisy": 0, "Rose": 0}
+inventory: dict = {}
 flower_map: dict = {"🌷 Tulip": "1", "🌼 Daisy": "2", "🌹 Rose": "3"}
-seed_map: dict = {"🌷🌱 Tulip seed": "1", "🌼🌱 Daisy seed": "2", "🌹🌱 Rose seed": "3"}
+seed_map = {"1": "🌷🌱 Tulip seed", "2": "🌼🌱 Daisy seed", "3": "🌹🌱 Rose seed"}
 wait_timers: dict = {"🌷": 3, "🌼": 5, "🌹": 10}
 wait_time: int = 0
 plot: str = "🟫"
@@ -15,24 +17,19 @@ money: int = 0
 
 
 def plant_seeds(seed: str, plot: str) -> str:  # returns updated plant plot emoji
-    
-    # Check if it's a valid seed number
-    if seed not in seed_map.values():
-        print("Invalid seed choice")
-        # Still needs to return plot (unchanged)
-        return plot
-    
     # Loop through the dictionary
     for key, value in seed_map.items():
         # Find the key matching the passed value (seed)
-        if value == seed:   
+        if key == seed:
             # Strip the string to get just the emoji
             # >strip makes a new list where each element is a word from the previous string
             # Set plot emoji to the new (flower) emoji
             # >removes the seedling and splits the strings ['🌷', ' Tulip seed']
             # >chooses string at index 0 for the emoji
-            plot = key.split("🌱")[0]
+            plot = value.split("🌱")[0]
             print(f"You planted a {plot}.")
+            # Get out of loop once planted the seed
+            break 
     return plot
 
 def grow_flower(plot: str) -> None:
@@ -43,7 +40,7 @@ def grow_flower(plot: str) -> None:
         print(".", end=" ", flush=True)
         time.sleep(1)
 
-def harvest_crops() -> str:
+def harvest_flowers() -> str:
     plot = "🟫"  # reset to empty soil
     # add flower to inventory
     return plot
@@ -54,37 +51,52 @@ while True:
     print(f"\n_______________\n[ {plot} ]\n", flush=True)
     
     print(
-        "What do you want to do? \n[1] Plant seeds \n[2] Pick flowers \n[3] Go to the market"
+        "What do you want to do? \n[1] Plant seeds \n[2] Pick flowers \n[3] Go to the market \n[4] Show inventory"
     )
-    menu_case: str = input("Choose an action (enter 1, 2 or 3):\n")
+    menu_case: str = input("Choose an action (enter 1, 2, 3 or 4):\n")
     
-    if menu_case not in ["1", "2", "3"]:
-        print("Invalid action choice. Please try again (enter 1, 2 or 3).")
+    if menu_case not in ["1", "2", "3", "4"]:
+        print("Invalid action choice. Please try again (enter 1, 2, 3 or 4).")
+        continue  # skip the rest of the loop and start over
     
-    if menu_case == "1" and plot == "🟫":     
+    # [1] Plant seeds
+    if menu_case == "1" and plot == "🟫":
         print("Choose a seed to plant:")
-        #check if any seeds are in inventory
         seed: str = input("Type '1' for Tulip seeds, '2' for Daisy seeds, '3' for Rose seeds: ")
-        # Set plot emoji to the new seed
-        plot = plant_seeds(seed, plot)
+        
+        # Check if it's a valid seed number by looking up seed_map keys
+        if seed not in seed_map.keys():
+            print("Invalid seed choice")
+        # Check if any seeds are in inventory
+        elif seed_map.get(seed) not in inventory:
+            print(f"You don't have any {seed_map.get(seed)} seeds!")
+        # Plant a seed if correct number
+        else:
+            # Set plot emoji to the new seed
+            plot = plant_seeds(seed, plot)
         
         # Growth timer
-        # >should it run from inside plant seeds?
-        grow_flower(plot)
-    else:
-        print("Pick your flower first!")    
+        if plot != "🟫":
+            grow_flower(plot)
+    elif menu_case == "1" and plot != "🟫":
+        print("Pick your flower first!")
         
-        
+    # [2] Pick flowers
     if menu_case == "2":  # harvest, add to inventory, return to empty soil,
-        plot = harvest_crops()  # reset plant to empty
+        plot = harvest_flowers()  # reset plant to empty
         # add flower to inventory
 
-
+    # [3] Go to the market
     if menu_case == "3":
         print("You're at the market. What do you want to do?")
         print("[1] Sell flowers \n[2] Buy seeds \n[3] Buy tools \n[4] Deliver bouquets"
               "\n[5] Go back to farm")
-        market_case: str = input("Choose an action: (Enter a number)")   # market_case is the action in the market
+        # market_case is the action in the market
+        market_case: str = input("Choose an action: (Enter a number)")
     
-    
+    # [4] Open Inventory    
+    if menu_case == "4":
+        # Show everything in inventory (can categorize later)
+        print(inventory)
+        # Go back to menu
         
